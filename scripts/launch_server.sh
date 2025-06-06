76ef49a2581fc0e593a5c2fa914a3bb354b921c4
Marti Umbert <marti.umbert@bsc.es>
2025-06-06 10:47:08 +0200

    scripts/launch_server.sh: MT test with CA-EN model

--- scripts/launch_server.sh
+++ scripts/launch_server.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
 
-MODEL=projecte-aina/faster-whisper-large-v3-ca-3catparla
+MODEL=projecte-aina/faster-whisper-large-v3-ca-3catparla # works fine
+# MODEL=projecte-aina/faster-whisper-large-v3-tiny-caesar
+# MODEL=large-v3
+# MODEL=large-v2
 PORT=8000
-MODEL_CASCADED_TRANSLATION=projecte-aina/aina-translator-ca-es
-LANGUAGE=ca
+# MODEL_CASCADED_TRANSLATION=projecte-aina/aina-translator-ca-es
+MODEL_CASCADED_TRANSLATION=projecte-aina/aina-translator-ca-en
+LANGUAGE=ca # ca
+BACKEND="faster-whisper" # "whisper-x" # faster-whisper, whisper-x
 
 cmd="whisperlivekit-server --model $MODEL \
                            --port $PORT \
                            --model_cascaded_translation $MODEL_CASCADED_TRANSLATION  \
-                           --language $LANGUAGE"
+                           --language $LANGUAGE \
+                           --backend $BACKEND \
+                           --buffer_trimming_sec 10"
 echo $cmd
 eval $cmd
